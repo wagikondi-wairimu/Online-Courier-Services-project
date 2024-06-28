8626ca8aa0333eaea2e8e246235a7494bf58ba2f
wagikondi-wairimu <judywagikondi@gmail.com>
2024-06-28 14:56:07 +0300

    Autowire repositories added

--- onlinecourier/src/main/java/com/example/onlinecourier/Service/ProductService.java
+++ onlinecourier/src/main/java/com/example/onlinecourier/Service/ProductService.java
@@ -1,6 +1,6 @@
 package com.example.onlinecourier.Service;
 
-import com.example.onlinecourier.Repository.ProductRepository;
+import com.example.onlinecourier.Repository.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -8,4 +8,19 @@ import org.springframework.stereotype.Service;
 public class ProductService {
     @Autowired
     private ProductRepository productRepository;
+    @Autowired
+    private OrderRepository orderRepository;
+    @Autowired
+    private PaymentRepository paymentRepository;
+    @Autowired
+    private CompanyRepository companyRepository;
+    @Autowired
+    private HubRateRepository hubRateRepository;
+    @Autowired
+    private DeliveryDetailsRepository deliveryDetailsRepository;
+    @Autowired
+    private  NonDeliveryDetailsRepository nonDeliveryDetailsRepository;
+    @Autowired
+    private ReviewRepository reviewRepository;
+
 }
