813234316b99f4c55754cee60f45b2c6eca02748
wagikondi-wairimu <judywagikondi@gmail.com>
2024-06-28 11:12:37 +0300

    application.properties added

---
+++ onlinecourier/src/main/java/com/example/onlinecourier/OnlinecourierApplication.java
@@ -0,0 +1,13 @@
+package com.example.onlinecourier;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlinecourierApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlinecourierApplication.class, args);
+	}
+
+}
---
+++ onlinecourier/src/test/java/com/example/onlinecourier/OnlinecourierApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.onlinecourier;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlinecourierApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
